013af11dd9b71ff63f3643f0d8d22dc0c92a159f
Suresh Rupasinghe <suresh.thivanka@gmail.com>
2020-01-02 15:47:58 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from './utils/theme'
+import GlobalStyles from './utils/global'
+
+import App from './App';
+
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ThemeProvider theme = {theme}>
+            <>
+            <App />
+            <GlobalStyles />
+            </>
+        </ThemeProvider>
+    </BrowserRouter>,
+document.getElementById('root'));
