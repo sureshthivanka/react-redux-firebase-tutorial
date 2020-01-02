84e0ddbc3db247e2ad7b43c15eeee06d84364ed0
Suresh Rupasinghe <suresh.thivanka@gmail.com>
2020-01-02 15:46:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Layout from './hoc/layout/Layout';
+import Home from './containers/Home/Home';
+import Todos from './containers/Todos/Todos';
+
+const App = () => {
+    return (
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/todos" component={Todos} />
+            <Redirect to='/' />
+          </Switch>
+            
+        </Layout>
+    );
+};
+
+export default App;
